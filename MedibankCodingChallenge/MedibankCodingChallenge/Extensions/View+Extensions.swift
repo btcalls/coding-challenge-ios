b5b0ebc123d98c3f34ce3eaa57913aab0cae7f0d
Jason Jon Carreos <jasondev06@gmail.com>
2026-02-11 20:50:32 +1100

    Added EmptyViewModifier

---
+++ MedibankCodingChallenge/MedibankCodingChallenge/Extensions/View+Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  View+Extensions.swift
+//  MedibankCodingChallenge
+//
+//  Created by Jason Jon Carreos on 11/2/2026.
+//
+
+import SwiftUI
+
+extension View {
+    /// Modifier to display a `ContentUnavailableView` given the condition is fulfilled.
+    /// - Parameters:
+    ///   - type: The type of empty content view to display.
+    ///   - condition: The condition to display the view.
+    ///   - label: `Label` that makes up the main title of the view.
+    ///   - description: Optional. The description displayed as a configured view.
+    ///   - actions: Optional. Actions to display along the view.
+    /// - Returns: Modified view with unavailable view option.
+    func emptyView<Label, Description, Actions>(
+        if condition: Bool,
+        type: EmptyViewType = .generic,
+        label: Label,
+        @ViewBuilder description: () -> Description = EmptyView.init,
+        @ViewBuilder actions: () -> Actions = EmptyView.init
+    ) -> some View where Label : View, Description: View, Actions : View  {
+        return modifier(EmptyViewModifier(for: type,
+                                          if: condition,
+                                          label: label,
+                                          description: description,
+                                          actions: actions))
+    }
+}
---
+++ MedibankCodingChallenge/MedibankCodingChallenge/Screens/Headlines/HeadlinesView.swift
@@ -0,0 +1,18 @@
+//
+//  HeadlinesView.swift
+//  MedibankCodingChallenge
+//
+//  Created by Jason Jon Carreos on 11/2/2026.
+//
+
+import SwiftUI
+
+struct HeadlinesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeadlinesView()
+}
